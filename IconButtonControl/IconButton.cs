ef2db53ef89527c5622b7eb17e5e7e9008452b07
AlexeyGer <spochteniem@gmail.com>
2021-06-10 14:30:13 +0300

    Add ResourceDictionary and Demo Application

--- IconButtonControl/IconButton.cs
+++ IconButtonControl/IconButton.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using IconButtonControl.Enums;
 
 namespace IconButtonControl
@@ -55,6 +45,15 @@ namespace IconButtonControl
 		public IconButton()
 		{
 			this.SetCurrentValue(IconPositionProperty, IconPosition.Left);
+		}
+
+		public static readonly DependencyProperty TextProperty =
+			DependencyProperty.Register(nameof(Text), typeof(string), typeof(IconButton), new PropertyMetadata());
+
+		public string Text
+		{
+			get { return (string)GetValue(TextProperty); }
+			set { SetValue(TextProperty, value); }
 		}
 
 		public static readonly DependencyProperty IconSourceProperty =
